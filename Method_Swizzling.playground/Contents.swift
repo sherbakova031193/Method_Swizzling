2f393e3276ad201ce3d76775bef9b75977f5607c
Елизавeта <sherbakova031193@gmail.com>
2021-07-25 12:18:10 +0300

    Method Swizzling

--- Method_Swizzling.playground/Contents.swift
+++ Method_Swizzling.playground/Contents.swift
@@ -1,2 +1,34 @@
 import UIKit
 
+class SomeClass {
+    
+    @objc dynamic class func saveSomeThings(_ someThing: String) {
+        print("SomeClass", someThing)
+    }
+}
+
+class SomeTestClass {
+    
+    @objc dynamic class func forTesting_saveSomeThings(_ someThing: String) {
+        print("SomeTestClass", someThing)
+    }
+    
+    static let originalMethod = class_getClassMethod(SomeClass.self, #selector(SomeClass.saveSomeThings(_ :)))
+    
+    static let swizzledMethod = class_getClassMethod(SomeTestClass.self, #selector(SomeTestClass.forTesting_saveSomeThings(_ :)))
+    
+    static func start() {
+        method_exchangeImplementations(originalMethod!, swizzledMethod!)
+    }
+    
+    static func stop() {
+        method_exchangeImplementations(swizzledMethod!, originalMethod!)
+    }
+}
+
+SomeClass.saveSomeThings("saveSomeThings")
+
+SomeTestClass.start()
+SomeClass.saveSomeThings("saveSomeThings")
+SomeTestClass.stop()
+SomeClass.saveSomeThings("saveSomeThings")
